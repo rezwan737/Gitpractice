ec1dab49c83397d43610f0aa5f4b265dd5ae6ef2
Grim <felony_jack@yahoo.com>
2020-10-21 23:03:51 -0400

    My first commit

---
+++ ClassA.java
@@ -0,0 +1,14 @@
+
+public class ClassA {
+
+	static float x = 5;
+	static float y = 3;
+	
+	public static void Addition() {
+		
+		float z = x + y;{
+			System.out.println("Addition of x and y= "+z);
+		}
+	}
+	
+}
